f4edfe9fd51235a260c2b182e480b3b8900502f8
Matheus Almeida <almeidadrk@outlook.com>
2022-10-24 15:36:22 -0300

    Ajuste do void

--- Test AV2.cpp
+++ Test AV2.cpp
@@ -6,59 +6,59 @@
 
 //tentativa de implementar um menu para especialidades
 void specialist () {
-    printf("1 - Clinica Geral");
-    printf("2 - Cardiologia");
-    printf("3 - Oftalmologia");
-    printf("4 - Pediatria");
-    printf("5 - Ortopedia e Traumatologia");
-    printf("6 - Neurologia e Psiquiatria");
-    printf("7 - Ginecologia e Obstetricia");
+    printf("1 - Clinica Geral\n");
+    printf("2 - Cardiologia\n");
+    printf("3 - Oftalmologia\n");
+    printf("4 - Pediatria\n");
+    printf("5 - Ortopedia e Traumatologia\n");
+    printf("6 - Neurologia e Psiquiatria\n");
+    printf("7 - Ginecologia e Obstetricia\n");
 }
 void specialistMedicos1 () {
-    printf("1 - Clinica Geral - 1 Dr. Huberto Castro");
-    printf("1 - Clinica Geral - 2 Drª. Liandra Cerqueira");
-    printf("1 - Clinica Geral - 3 Dr. Magno Florencio");
+    printf("1 - Clinica Geral - 1 Dr. Huberto Castro\n");
+    printf("1 - Clinica Geral - 2 Drï¿½. Liandra Cerqueira\n");
+    printf("1 - Clinica Geral - 3 Dr. Magno Florencio\n");
 }
 void specialistMedicos2 () {
-    printf("2 - Cardiologia - 1 Dr. Pedro Paulo Souza");
-    printf("2 - Cardiologia - 2 Dr. Victor Almeida");
-    printf("2 - Cardiologia - 3 Dr. Robert de Jesus");
+    printf("2 - Cardiologia - 1 Dr. Pedro Paulo Souza\n");
+    printf("2 - Cardiologia - 2 Dr. Victor Almeida\n");
+    printf("2 - Cardiologia - 3 Dr. Robert de Jesus\n");
 }
 void specialistMedicos3 () {
-    printf("3 - Oftalmologia - 1 Drª. Flavia Perelberg");
-    printf("3 - Oftalmologia - 2 Dr. Matheus Costa");
-    printf("3 - Oftalmologia - 3 Dr. Daniel da Cruz");
+    printf("3 - Oftalmologia - 1 Drï¿½. Flavia Perelberg\n");
+    printf("3 - Oftalmologia - 2 Dr. Matheus Costa\n");
+    printf("3 - Oftalmologia - 3 Dr. Daniel da Cruz\n");
 }
 void specialistMedicos4 () {
-    printf("4 - Pediatria - 1 Drª. Alderiza Costa");
-    printf("4 - Pediatria - 2 Drª. Lis Alphontes");
-    printf("4 - Pediatria - 3 Dr. Luiz Dahora");
+    printf("4 - Pediatria - 1 Drï¿½. Alderiza Costa\n");
+    printf("4 - Pediatria - 2 Drï¿½. Lis Alphontes\n");
+    printf("4 - Pediatria - 3 Dr. Luiz Dahora\n");
 }
 void specialistMedicos5 () {
-    printf("5 - Ortopedia e Traumatologia - 1 Dr. Huberto Celestim");
-    printf("5 - Ortopedia e Traumatologia - 2 Drª. Amanda Almeida");
-    printf("5 - Ortopedia e Traumatologia - 3 Dr. Hildebrando Caetano");
+    printf("5 - Ortopedia e Traumatologia - 1 Dr. Huberto Celestim\n");
+    printf("5 - Ortopedia e Traumatologia - 2 Drï¿½. Amanda Almeida\n");
+    printf("5 - Ortopedia e Traumatologia - 3 Dr. Hildebrando Caetano\n");
 }
 void specialistMedicos6 () {
-    printf("6 - Neurologia e Psiquiatria - 1 Drº. Talita Rocha");
-    printf("6 - Neurologia e Psiquiatria - 2 Dr. Amorim");
-    printf("6 - Neurologia e Psiquiatria - 3 Dr. Joaquim");
+    printf("6 - Neurologia e Psiquiatria - 1 Drï¿½. Talita Rocha\n");
+    printf("6 - Neurologia e Psiquiatria - 2 Dr. Amorim\n");
+    printf("6 - Neurologia e Psiquiatria - 3 Dr. Joaquim\n");
 }
 void specialistMedicos7 () {
-    printf("7 - Ginecologia e Obstetricia - 1 Dr. Celso Rodrigues");
-    printf("7 - Ginelocogia e Obstetricia - 2 Drª. Lorena Viega");
-    printf("7 - Ginecologia e Obstetricia - 3 Drª. Patricia Lima");
+    printf("7 - Ginecologia e Obstetricia - 1 Dr. Celso Rodrigues\n");
+    printf("7 - Ginelocogia e Obstetricia - 2 Drï¿½. Lorena Viega\n");
+    printf("7 - Ginecologia e Obstetricia - 3 Drï¿½. Patricia Lima\n");
 }
-//tentativa de implementação de banco de dados simples em C
+//tentativa de implementaï¿½ï¿½o de banco de dados simples em C
 
 main(){
 setlocale(LC_ALL,"");
 
 int numspecilist, nummedico;
-    printf("Olá, Bem Vindo ao nosso prototipo de APP\n");
+    printf("Olï¿½, Bem Vindo ao nosso prototipo de APP\n");
     
-    
-    printf("Escolha o numero da opção de sua especialidade: ");
+    specialist();
+    printf("Escolha o numero da opï¿½ï¿½o de sua especialidade: ");
     numspecilist = getche();
 
 	return 0;
